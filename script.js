fc0e12667da5941279b7dbaddb0d6902d04d4871
Marcin Suski <114236312+marcinsuski@users.noreply.github.com>
2022-11-04 15:09:49 +0100

    files added
    full timer with miliseconds
    - play
    - pause
    - reset

---
+++ script.js
@@ -0,0 +1,98 @@
+// Variables for buttons
+const startStopBtn = document.querySelector("#startStopBtn");
+const resetBtn = document.querySelector("#resetBtn");
+
+//Variables for time values
+let miliseconds = 0;
+let seconds = 0;
+let minutes = 0;
+let hours = 0;
+
+// Variables for leading zero
+
+let leadingMiliseconds = 0;
+let leadingSeconds = 0;
+let leadingMinutes = 0;
+let leadingHours = 0;
+
+//Variables for set interval & timerstatus
+
+let timerInterval = null;
+let timerStatus = "stopped";
+
+// Stop Watch Function
+
+function stopWatch() {
+    miliseconds++;
+
+    if (miliseconds / 100 === 1) {
+        miliseconds = 0;
+        seconds++;
+
+        if (seconds / 60 === 1) {
+            seconds = 0;
+            minutes++;
+
+            if (minutes / 60 === 1) {
+                minutes = 0;
+                hours++;
+            }
+        }
+    }
+
+    if (miliseconds < 10) {
+        leadingMiliseconds = "0" + miliseconds.toString();
+    } else {
+        leadingMiliseconds = miliseconds;
+    }
+    if (seconds < 10) {
+        leadingSeconds = "0" + seconds.toString();
+    } else {
+        leadingSeconds = seconds;
+    }
+    if (minutes < 10) {
+        leadingMinutes = "0" + minutes.toString();
+    } else {
+        leadingMinutes = minutes;
+    }
+    if (hours < 10) {
+        leadingHours = "0" + hours.toString();
+    } else {
+        leadingHours = hours;
+    }
+
+    let displayTimer = (document.getElementById("timer").innerText =
+        leadingHours +
+        ":" +
+        leadingMinutes +
+        ":" +
+        leadingSeconds +
+        ":" +
+        leadingMiliseconds);
+}
+
+startStopBtn.addEventListener("click", function () {
+    if (timerStatus === "stopped") {
+        timerInterval = window.setInterval(stopWatch, 10);
+        document.getElementById(
+            "startStopBtn"
+        ).innerHTML = `<i class="fa-solid fa-pause" id="pause"></i>`;
+        timerStatus = "started";
+    } else {
+        window.clearInterval(timerInterval);
+        document.getElementById(
+            "startStopBtn"
+        ).innerHTML = `<i class="fa-solid fa-play" id="play"</i>`;
+        timerStatus = "stopped";
+    }
+});
+
+resetBtn.addEventListener("click", function () {
+    window.clearInterval(timerInterval);
+    miliseconds = 0;
+    seconds = 0;
+    minutes = 0;
+    hours = 0;
+
+    document.getElementById("timer").innerHTML = "00:00:00:00";
+});
